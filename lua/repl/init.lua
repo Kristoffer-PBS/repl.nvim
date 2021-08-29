5db2382a60fb937815766406bc27ba2a05d66415
Kristoffer-PBS <57013304+Kristoffer-PBS@users.noreply.github.com>
2021-08-29 13:05:51 +0200

    feat: MWP implemented, not tested yet.

---
+++ lua/repl/init.lua
@@ -0,0 +1,57 @@
+local fn = vim.fn
+
+local M = {}
+
+local bufnr_to_terminal_job_id_mapping = {}
+
+local get_terminal_job_id = function()
+	local bufnr = fn.bufnr("%")
+	local terminal_job_id = bufnr_to_terminal_job_id_mapping[tostring(bufnr)]
+	if not terminal_job_id then
+		error("terminal_job_id is nil")
+	end
+	return terminal_job_id
+end
+
+---
+---@param repl string
+M.start_repl_and_link_it_with_current_buffer = function(repl)
+	local bufnr = fn.bufnr("%")
+	vim.cmd("vsplit | terminal " .. repl)
+	local terminal_job_id = vim.b.terminal_job_id
+	if not terminal_job_id then
+		error("terminal_job_id is nil")
+	end
+	bufnr_to_terminal_job_id_mapping[tostring(bufnr)] = terminal_job_id
+    vim.cmd("command! -buffer ReplSendLineAtCursor    lua require('repl').send_line_at_cursor()")
+    vim.cmd("command! -buffer ReplSendVisualSelection lua require('repl').send_visual_selection()")
+    vim.cmd("command! -buffer ReplSendEntireBuffer    lua require('repl').send_entire_buffer()")
+end
+
+M.send_line_at_cursor = function()
+	local line_at_cursor = fn.getline(".")
+	local terminal_job_id = get_terminal_job_id()
+	fn.chansend(terminal_job_id, { line_at_cursor .. "\r" })
+end
+
+-- stylua: ignore
+M.send_visual_selection = function()
+	local terminal_job_id = get_terminal_job_id()
+	local lines           = require("utils").get_visual_selection()
+	lines[#lines]         = lines[#lines] .. "\r"
+	fn.chansend(terminal_job_id, lines)
+end
+
+-- stylua: ignore
+M.send_entire_buffer = function()
+    local terminal_job_id = get_terminal_job_id()
+    local lines           = fn.getline(1, "$")
+    lines[#lines]         = lines[#lines] .. "\r"
+    fn.chansend(terminal_job_id, lines)
+end
+
+M.setup = function(opts)
+    vim.cmd("command! -nargs=1 ReplStart lua require('repl').start_repl_and_link_it_with_current_buffer(<args>)")
+end
+
+return M
